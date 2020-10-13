ef6d07b0913d5314369fd85e845414e18cfc312a
Daniel Selase Anyidoho <dselasea@gmail.com>
2020-10-13 16:10:50 +0000

    upddate to ui and data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,36 +7,35 @@ function Weather() {
     condition: "",
     icon: "",
   });
-  const location = weather.location;
-  const city = weather.city;
-  const condition = weather.condition;
-  const icon = weather.icon;
-  const [searchInput, setSearchInput] = useState("ghana");
+  const { location, city, condition, icon } = weather;
+  const [searchInput, setSearchInput] = useState("");
   const [weatherNow, setWeatherNow] = useState([]);
   const [loader, setLoader] = useState(true);
+  const [country, setCountry] = useState([]);
 
   useEffect(() => {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=1b762a27694a99c88c292de5c5793d7d`
     )
       .then((response) => {
-        if(response.status >= 200 && response.status <= 299){
-          return response.json()
-        }else{
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
           setLoader(false);
-          throw new Error(response.statusText)
+          throw new Error(response.statusText);
         }
       })
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setWeatherNow(data);
         setLoader(false);
       })
-      .catch(err => {console.log("Error Loading Data")
+      .catch((err) => {
+        console.log("Error Loading Data");
       });
   }, [searchInput]);
 
-
+  // Get Weather Information from search input
   function getWeatherInfo(e) {
     if (searchInput === "") {
       console.log("Empty");
@@ -47,17 +46,32 @@ function Weather() {
           location: weatherNow.sys.country,
           city: weatherNow.name,
           condition: weatherNow.weather[0]["main"],
-          icon: "http://openweathermap.org/img/w/" +weatherNow.weather[0]["icon"] + ".png",
+          icon:
+            "http://openweathermap.org/img/w/" +
+            weatherNow.weather[0]["icon"] +
+            ".png",
         };
       });
-      // setLoader(true);
+    }
+    // Store Country
+    const countryList = { country: searchInput };
+    if (country.length < 5) {
+      setCountry((country) => {
+        return [...country, countryList];
+      });
+    } else {
+      return false;
     }
     e.preventDefault();
   }
 
-
-  if(loader){
-    return <div><h1>Loading...</h1></div>
+  // Render Loading JSX
+  if (loader) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
   return (
     <div>
@@ -69,10 +83,13 @@ function Weather() {
             placeholder="Search Country"
             value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
+            autoComplete="off"
           />
         </form>
         {searchInput === "" ? <p>Please enter a country</p> : null}
-        <button onClick={getWeatherInfo}>Search</button>
+        <button onClick={getWeatherInfo} type="submit">
+          Search
+        </button>
         <div className="weather-info">
           <div>
             <p>
@@ -87,6 +104,13 @@ function Weather() {
             <img src={icon} alt="weather icon" />
           </div>
         </div>
+        {country.map((count, index) => {
+          return (
+            <div key={index}>
+              <p>{count.country}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
